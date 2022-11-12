f51680d3ae9d28b9ed44144f236ba2092b6ee46c
Nicolas Clementi <clementinicolas@gmail.com>
2022-11-12 14:48:47 -0300

    middle y filtros  basico

--- peliculasApi/Controllers/GenerosController.cs
+++ peliculasApi/Controllers/GenerosController.cs
@@ -1,23 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using peliculasApi.Entidades;
+using peliculasApi.Filtros;
 using peliculasApi.Repositorios;
 
 namespace peliculasApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GenerosController: ControllerBase
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class GenerosController : ControllerBase
     {
         private readonly IRepositorio repositorio;
         private readonly ILogger<GenerosController> logger;
 
-        public GenerosController(IRepositorio repositorio,ILogger<GenerosController>logger)
+        public GenerosController(IRepositorio repositorio, ILogger<GenerosController> logger)
         {
             this.repositorio = repositorio;
             this.logger = logger;
         }
 
         [HttpGet]
+        //[ResponseCache(Duration=60)]
+        [ServiceFilter(typeof(FiltroDeAccion))]
         public ActionResult<List<Genero>> Get()
         {
             logger.LogInformation("Vamos a mostrar los generos");
@@ -32,6 +38,7 @@ namespace peliculasApi.Controllers
             var genero = await repositorio.ObtenerPorId(Id);
             if( genero == null)
             {
+                throw new ApplicationException($"El genero de id {Id} no fue encontrado");
                 logger.LogWarning($"No se encontro el genero con id: {Id}");
                 return NotFound();
             }
--- peliculasApi/Program.cs
+++ peliculasApi/Program.cs
@@ -1,16 +1,58 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Logging;
+using peliculasApi.Filtros;
 using peliculasApi.Repositorios;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddTransient<FiltroDeAccion>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+builder.Services.AddResponseCaching();
 // Add services to the container.
 builder.Services.AddTransient<IRepositorio ,RepositorioEnMemoria>();
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(FiltroDeExcepcion));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
+
+
+//app.Use(async (context, next) =>
+//{
+//    using (var swapStream = new MemoryStream())
+//    {
+//        var respuestaOriginal = context.Response.Body;
+//        context.Response.Body = swapStream;
+
+//        await next.Invoke();
+
+//        swapStream.Seek(0, SeekOrigin.Begin);
+//        string respuesta = new StreamReader(swapStream).ReadToEnd();
+//        swapStream.Seek(0,SeekOrigin.Begin);
+
+//        await swapStream.CopyToAsync(respuestaOriginal);
+//        context.Response.Body = respuestaOriginal;
+
+//        Console.WriteLine(respuesta);
+//    }
+//});
+
+
+app.Map("/mapa1", (app) =>
+{
+    app.Run(async context =>
+    {
+    await context.Response.WriteAsync("Estoy interceptando el pipeline");
+    });
+
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,6 +62,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseResponseCaching();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
